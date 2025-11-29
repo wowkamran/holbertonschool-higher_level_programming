ebccf9a91181ddd36809bdfb65c621296197f681
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-29 13:53:08 +0000

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Module for the read_file function, which reads a UTF-8 text file
+and prints its contents to stdout.
+"""
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its contents to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
