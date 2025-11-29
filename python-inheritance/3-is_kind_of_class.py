e56f56db31b930abfd8cb756a4eab8a57fe2d732
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-29 09:17:35 +0000

    Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines a function to check if an object is an instance
+of a class or its subclass.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if obj is an instance of a_class or a subclass of it."""
+    return isinstance(obj, a_class)
