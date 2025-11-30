c198ef3263098caf7f401e09910644ffd5903606
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 17:36:28 +0000

    Genre ID for all shows

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,6 @@
--- Script that lists all TV shows with their genre_id, including shows without a genre
--- Each record shows tv_shows.title - tv_show_genres.genre_id
--- Results sorted by tv_shows.title and tv_show_genres.genre_id
+-- 11-genre_id_all_shows.sql
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
