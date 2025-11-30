aa8ae110e310264bfdabf419bb5a43db6faaca3b
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 17:39:01 +0000

    List shows and genres

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,10 +1,8 @@
--- Script that lists all shows and their genres in the database hbtn_0d_tvshows
--- Display NULL for shows without a genre
--- Each record shows tv_shows.title - tv_genres.name, sorted by title and genre name
-SELECT tv_shows.title, genres.name
+-- 16-shows_by_genre.sql
+SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT JOIN genres
-ON tv_show_genres.genre_id = genres.id
-ORDER BY tv_shows.title ASC, genres.name ASC;
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;
