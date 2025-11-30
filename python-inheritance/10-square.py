28b403e075356543aa5b83ddbfe13df2787fd491
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 18:49:51 +0000

    10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Module that defines a Square class inheriting from Rectangle."""
-
-
-Rectangle = __import__('8-rectangle').Rectangle
+"""Defines a Square class that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """Square class inheriting from Rectangle."""
 
     def __init__(self, size):
-        """Initialize a Square with size."""
+        """Initialize Square with validated size."""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """Return the area of the square."""
+        """Return area of the square."""
         return self.__size * self.__size
