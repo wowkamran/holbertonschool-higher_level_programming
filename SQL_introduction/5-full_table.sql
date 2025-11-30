ee9e78c1a4c136f4f456b38ff597eb77518bc061
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 15:25:53 +0000

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
--- Script that prints the description of the table first_table
--- Use SHOW COLUMNS to display table structure
-SHOW COLUMNS FROM first_table;
+-- bir
+-- iki
+SHOW CREATE TABLE first_table;
