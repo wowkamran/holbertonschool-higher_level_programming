3f8f245a0bef4dc9a959e25ba7c13780147cc387
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 17:38:27 +0000

    Only Comedy

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,9 @@
--- Script that lists all Comedy shows in the database hbtn_0d_tvshows
--- Each record shows tv_shows.title, sorted by title ascending
+-- 15-comedy_only.sql
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN genres
-ON tv_show_genres.genre_id = genres.id
-WHERE genres.name = 'Comedy'
+ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
