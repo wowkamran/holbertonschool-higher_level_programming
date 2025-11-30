43f04dd792c7b14c94ab88c48894180d55db403a
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 15:16:41 +0000

    First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Script that creates a table called first_table
+-- Table has columns: id INT, name VARCHAR(256)
+-- Create table only if it does not already exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
