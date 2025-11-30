f3cc2c2eba92760b2093620ad96d6b1de4d3b8c1
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 17:38:00 +0000

    My genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,9 @@
--- Script that lists all genres of the TV show Dexter
--- Each record shows tv_genres.name, sorted by name ascending
-SELECT genres.name
-FROM genres
+-- 14-my_genres.sql
+SELECT tv_genres.name
+FROM tv_genres
 JOIN tv_show_genres
-ON genres.id = tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows
-ON tv_show_genres.tv_show_id = tv_shows.id
+ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY genres.name ASC;
+ORDER BY tv_genres.name ASC;
