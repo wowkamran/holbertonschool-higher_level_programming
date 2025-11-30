e81486aab256a22277f8e58d6c4f89d40b07d164
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 17:30:31 +0000

    Genre ID by show

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,8 @@
--- Script that lists all TV shows with at least one genre
--- Each record shows tv_shows.title - tv_show_genres.genre_id
--- Results sorted by tv_shows.title and tv_show_genres.genre_id
+-- script lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record displays tv_shows.title - tv_show_genres.genre_id.
+-- Results sortes in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- It uses only one SELECT statement.
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_show_genres.tv_show_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
