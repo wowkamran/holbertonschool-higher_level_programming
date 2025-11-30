d5aab6ae4480f29c129798fed0068eb47e46778f
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 17:31:35 +0000

    Root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Script that creates the MySQL user user_0d_1 with all privileges
--- Password is set to 'user_0d_1_pwd' and script does not fail if user exists
+-- 1-create_user.sql
+-- Create user user_0d_1 with full privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
