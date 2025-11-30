67e226cd08eb64d87dfdf396dbf749237052db39
wowkamran <K0mr0n.Ag@gmail.com>
2025-11-30 17:32:19 +0000

    ID can't be null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,5 @@
--- Script that creates the table id_not_null
--- Table has columns: id INT with default value 1, name VARCHAR(256)
--- Create table only if it does not already exist
+-- rtyuhgvbnmklkjhgfdfg
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
     name VARCHAR(256)
+);
